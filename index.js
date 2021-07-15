a02f8d45cf2f84180d7039e0807015f201ee4176
Damir <https://gitlab.com/Kakharov_Damir>
2021-07-15 14:05:40 +0300

    add metricks

---
+++ index.js
@@ -0,0 +1,32 @@
+let counter = new Counter();
+
+counter.init(
+  "8445cf97-e4f6-4a7d-8d9b-64c19b9c9a7d",
+  String(Math.random()).substr(2, 12),
+  "main"
+);
+
+counter.setAdditionalParams({
+  env: "production",
+  platform: navigator.userAgentData.platform,
+});
+
+counter.send(
+  "connect",
+  performance.timing.connectEnd - performance.timing.connectStart
+);
+
+counter.send(
+  "ttfb",
+  performance.timing.responseEnd - performance.timing.requestStart
+);
+
+counter.send("language", navigator.language);
+
+counter.send("connection-type", navigator.connection.type);
+
+const button = document.getElementById("coming-soon-button");
+
+button.addEventListener("click", () => {
+  counter.send("click-button", +new Date()) - performance.timeOrigin;
+});
